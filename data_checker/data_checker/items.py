195a889372c24f22616a3683173c56a2293e095c
Damian Tykalowski <d47zm3@gmail.com>
2019-11-22 10:42:29 +0100

    added data checker using scrapy

---
+++ data_checker/data_checker/items.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class Dataset(scrapy.Item):
+    name = scrapy.Field()
+    link = scrapy.Field()
+    organization = scrapy.Field()
